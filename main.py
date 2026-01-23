0503322ccd03a3095cb6bc38093fc0e3a0ee45f4
app <you@example.com>
2026-01-23 15:51:27 +0530

    restructure the README.me

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
-
 import os
 from flask import Flask, render_template
 
